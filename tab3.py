6acf41f08b071d9a2fe5aa2c24b030c57544de4c
Damian Mroz <damianmroz141@gmail.com>
2024-06-14 15:09:48 +0200

    Modify tab3.py

--- tab3.py
+++ tab3.py
@@ -2,17 +2,15 @@ from dash import dcc
 from dash import html
 import plotly.graph_objects as go
 import plotly.express as px
-import string
+from dash import dash_table
 
 def render_tab(df_days,df_customer):
     fig = px.bar(df_customer, x="Store_type", y="count", color="Gender", title="Customer information")
-    translation_table = str.maketrans('', '', string.digits)
-    df_days=df_days[['Store_type','day']].to_string()
     n1 = "\n"
     layout = html.Div([html.H1('Sklepy',style={'text-align':'center'}),
                        html.Div([html.Div([dcc.Graph(id='customer',figure=fig)],style={'width':'50%'}),
-                       html.Div([dcc.Textarea(
-                       value=f'Table that shows days with the most transactions: {n1} {n1} {df_days.translate(translation_table)}',
-                       style={'width': '100%'})])])])
+                       html.Label("Table that shows the days with the most transactions and the corresponding total amout of transactions",style={"font-weight": "bold"}),
+                       html.Div([dash_table.DataTable(df_days.to_dict('records'),[{"name":i, "id":i} for i in df_days.columns],style_cell={'textAlign': 'left'})])       
+                       ])])
     
     return layout
